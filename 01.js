587d87062a2d08d20981a8b79e6b35ed6e2ccaea
LuceroRamos <141787995+kramosparedes@users.noreply.github.com>
2023-09-08 12:09:38 -0500

    prueba

--- 01.js
+++ 01.js
@@ -11,7 +11,8 @@ function soloNumeros(array) {
   // soloNumeros([1, 'Henry', 2]) debe retornar [1, 2]
 
   // Tu código aca:
-
+  function soloNumeros(array) { 
+}
 }
 
 // No modifiques nada debajo de esta linea //
